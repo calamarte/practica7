2704ae9e72dd1c883deaf63a47289d99f597a5fb
dgarcia0 <davidgarcia227@gmail.com>
2017-05-23 18:17:29 +0200

    pls

---
+++ src/Inicio.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by Dgarcia on 23/05/2017.
+ */
+public class Inicio {
+    private JComboBox Lector;
+    private JComboBox Prestamos;
+    private JComboBox Libros;
+    private JComboBox Autores;
+    private JComboBox Sanciones;
+}
