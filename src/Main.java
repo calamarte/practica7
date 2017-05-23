ac0001e3a1ab91851318db73ff4ac0574d503610
calamarte <calamarte@gmail.com>
2017-05-23 17:55:34 +0200

    actu

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,16 @@ import javax.swing.*;
  * Created by calamarte on 23/05/2017.
  */
 public class Main {
+    static JFrame frame = new JFrame("Login");
+
     public static void main(String[] args) {
         Login l = new Login();
-        JFrame frame = new JFrame("Login");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 200);
         frame.setVisible(true);
 
 
         frame.setContentPane(l.getLoginPanel());
+        //JOptionPane.showMessageDialog(frame,"Incorrecto","Tal",JOptionPane.WARNING_MESSAGE);
     }
 }
