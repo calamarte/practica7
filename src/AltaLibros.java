a04fd52f90f3fb9e29077d49f21166a237462c43
calamarte <calamarte@gmail.com>
2017-05-24 19:02:54 +0200

    probando con nuevos panel

---
+++ src/AltaLibros.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+/**
+ * Created by calamarte on 24/05/2017.
+ */
+public class AltaLibros {
+    private JPanel AltaLibrosPanel;
+    private JButton cancel;
+    private JButton ok;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+
+    public JPanel getAltaLibrosPanel() {
+        return AltaLibrosPanel;
+    }
+}
