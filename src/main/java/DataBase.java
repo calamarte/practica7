11c26f93b7443eda9f599b12a1999690d53d0247
dgarcia0 <davidgarcia227@gmail.com>
2017-05-31 18:06:19 +0200

    prueba select

--- src/main/java/DataBase.java
+++ src/main/java/DataBase.java
@@ -1,6 +1,8 @@
 import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 
 /**
@@ -10,18 +12,15 @@ public class DataBase {
     String servidor;
     String usuario;
     String passwd;
-
-
-
+    Connection conn;
 
     public DataBase(String server, String username, String password) throws Exception {
-
-        Connection conn = null;
         try {
             conn =
                     DriverManager.getConnection("jdbc:mysql://"+server+"/?" +
                             "user="+username+"&password="+password);
-            // Do something with the Connection
+
+
 
         } catch (SQLException ex) {
             // handle any errors
@@ -35,4 +34,16 @@ public class DataBase {
 
     }
 
+    public boolean login(String bibliotecario, String password)throws Exception {
+        Statement stmt = null;
+        ResultSet rs = null;
+        stmt = conn.createStatement();
+        rs = stmt.executeQuery("SELECT count(*) FROM Bibliotecario where Nombre = "+usuario+"and Password = "+password);
+        System.out.println(rs);
+
+        if(rs.equals("1")){
+            return true;
+        }
+        else return false;
+    }
 }
