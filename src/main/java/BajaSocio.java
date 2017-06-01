0d613de373251d49813da03820324b06791e6740
dgarcia0 <davidgarcia227@gmail.com>
2017-06-01 17:28:53 +0200

    inicio bajas

---
+++ src/main/java/BajaSocio.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by Dgarcia on 01/06/2017.
+ */
+public class BajaSocio {
+    private JComboBox comboBox1;
+    private JTextField textField1;
+    private JTable table1;
+    private JButton button1;
+    private JButton button2;
+}
