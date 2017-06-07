834f11f9a24a68893a8e7eb2a4187ff6e5208e18
calamarte <calamarte@gmail.com>
2017-06-07 17:23:48 +0200

    el gettereewgfasdsagv<fad

--- src/main/java/BajaSocio.java
+++ src/main/java/BajaSocio.java
@@ -4,9 +4,14 @@ import javax.swing.*;
  * Created by Dgarcia on 01/06/2017.
  */
 public class BajaSocio {
-    private JButton cancelarButton;
-    private JButton eliminarButton;
+    private JButton button1;
+    private JButton button2;
     private JComboBox comboBox1;
     private JTextField textField1;
     private JButton buscarButton;
+    private JPanel BajaSocioPanel;
+
+    public JPanel getBajaSocioPanel() {
+        return BajaSocioPanel;
+    }
 }
